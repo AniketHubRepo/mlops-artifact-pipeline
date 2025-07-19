ed2ebcd28218ba21c94b76f34cd14409c899847f
AniketHubRepo <Tndc@123>
2025-07-20 02:51:04 +0530

    Added inference.py

---
+++ src/inference.py
@@ -0,0 +1,25 @@
+import pickle
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, log_loss
+
+with open("model_train.pkl", "rb") as f:
+    # saved = pickle.load(f)
+    # model = saved['model']
+    # scaler = saved['scaler']
+    model = pickle.load(f)
+
+digits = load_digits()
+X, y = digits.data, digits.target
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+y_pred = model.predict(X_scaled)
+y_proba = model.predict_proba(X_scaled)
+
+print("Classification Report : \n")
+print(classification_report(y, y_pred))
+
+loss = log_loss(y, y_proba)
+print(f"\nLog Loss: {loss:.4f}")
